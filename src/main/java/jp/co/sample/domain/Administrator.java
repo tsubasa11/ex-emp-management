8b065da4429e1aa89e52ea66a3e5e19e97387ccb
tsubasa <tsubasa.namiki@rakus-partners.co.jp>
2020-02-05 10:23:22 +0900

    first

---
+++ src/main/java/jp/co/sample/domain/Administrator.java
@@ -0,0 +1,49 @@
+package jp.co.sample.domain;
+
+public class Administrator {
+	private Integer id;
+	private String name;
+	private String mailAdress;
+	private String password;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMailAdress() {
+		return mailAdress;
+	}
+
+	public void setMailAdress(String mailAdress) {
+		this.mailAdress = mailAdress;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "Administrator [id=" + id + ", name=" + name + ", mailAdress=" + mailAdress + ", password=" + password
+				+ ", getId()=" + getId() + ", getName()=" + getName() + ", getMailAdress()=" + getMailAdress()
+				+ ", getPassword()=" + getPassword() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ ", toString()=" + super.toString() + "]";
+	}
+
+}
