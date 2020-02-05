3efb56989dc53c90717361a3a17c3bf1ef0beb36
tsubasa <tsubasa.namiki@rakus-partners.co.jp>
2020-02-05 11:01:03 +0900

    insertメソッド追加

--- src/main/java/jp/co/sample/repository/AdministratorRepository.java
+++ src/main/java/jp/co/sample/repository/AdministratorRepository.java
@@ -2,7 +2,9 @@ package jp.co.sample.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import jp.co.sample.domain.Administrator;
@@ -17,9 +19,16 @@ public class AdministratorRepository {
 		Administrator administrator = new Administrator();
 		administrator.setId(rs.getInt("id"));
 		administrator.setName(rs.getString("name"));
-		administrator.setMailAdress(rs.getString("mailAdress"));
+		administrator.setMailAdress(rs.getString("mail_adress"));
 		administrator.setPassword(rs.getString("password"));
 		return administrator;
 	};
+	
+	public void insert(Administrator administrator) {
+		SqlParameterSource param = new BeanPropertySqlParameterSource(administrator);
+		String insertSql="insert into administrators (id,name,mail_adress,password) values(:id,:name,:mailAdress,:password)";
+		
+		template.update(insertSql, param);
+	}
 
 }
