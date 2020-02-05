e4aef29c42a7f65e151749cf0f6d5d06dc310ab1
tsubasa <tsubasa.namiki@rakus-partners.co.jp>
2020-02-05 10:38:10 +0900

    first

--- src/main/java/jp/co/sample/domain/Administrator.java
+++ src/main/java/jp/co/sample/domain/Administrator.java
@@ -1,10 +1,31 @@
 package jp.co.sample.domain;
 
 public class Administrator {
+
+	/** 従業員id */
 	private Integer id;
+
+	/** 従業員の名前 */
 	private String name;
+
+	/** 従業員のアドレス */
 	private String mailAdress;
+
+	/** 従業員のパスワード */
 	private String password;
+
+	/** 全ての情報を定義 */
+	public Administrator(Integer id, String name, String mailAdress, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.mailAdress = mailAdress;
+		this.password = password;
+	}
+
+	public Administrator() {
+
+	}
 
 	public Integer getId() {
 		return id;
