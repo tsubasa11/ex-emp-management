07c798310a34cff3a3a3bfa03368590285c1f526
tsubasa <tsubasa.namiki@rakus-partners.co.jp>
2020-02-05 10:46:51 +0900

    first

---
+++ src/main/java/jp/co/sample/repository/AdministratorRepository.java
@@ -0,0 +1,23 @@
+package jp.co.sample.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import jp.co.sample.domain.Administrator;
+
+@Repository
+public class AdministratorRepository {
+
+	@Autowired
+	NamedParameterJdbcTemplate template;
+	
+	private static final RowMapper<Administrator> ADMINISTRATOR_ROW_MAPPER=(rs,i)->{
+		Administrator administrator = new Administrator();
+		administrator.setId(rs.getInt("id"));
+		administrator.setName(rs.getString("name"));
+		administrator.setMailAdress(rs.getString("mailAdress"));
+		administrator.setPassword(rs.getString("password"));
+	};
+}
